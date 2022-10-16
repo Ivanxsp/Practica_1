08013b60c8de1140138c671ab26f6244e20731fd
Iván Santos <ivansperdosa@gmail.com>
2022-10-16 23:26:43 +0200

    js

---
+++ scripts.js
@@ -0,0 +1,60 @@
+function breda_d() {
+    document.getElementById("breda").style.zIndex="100";
+    document.getElementById("breda_i").style.zIndex='100';
+    document.getElementById("venus").style.zIndex="10";
+    document.getElementById("venus_i").style.zIndex='10';
+    document.getElementById("meninas").style.zIndex="10";
+    document.getElementById("meninas_i").style.zIndex='10';
+    document.getElementById("vulcano").style.zIndex="10";
+    document.getElementById("vulcano_i").style.zIndex='10';
+    document.getElementById("inocencio").style.zIndex="10";
+    document.getElementById("inocencio_i").style.zIndex='10';
+}
+function venus_d() {
+    document.getElementById("breda").style.zIndex="10";
+    document.getElementById("breda_i").style.zIndex='10';
+    document.getElementById("venus").style.zIndex="100";
+    document.getElementById("venus_i").style.zIndex='100';
+    document.getElementById("meninas").style.zIndex="10";
+    document.getElementById("meninas_i").style.zIndex='10';
+    document.getElementById("vulcano").style.zIndex="10";
+    document.getElementById("vulcano_i").style.zIndex='10';
+    document.getElementById("inocencio").style.zIndex="10";
+    document.getElementById("inocencio_i").style.zIndex='10';
+}
+function meninas_d() {
+    document.getElementById("breda").style.zIndex="10";
+    document.getElementById("breda_i").style.zIndex='10';
+    document.getElementById("venus").style.zIndex="10";
+    document.getElementById("venus_i").style.zIndex='10';
+    document.getElementById("meninas").style.zIndex="100";
+    document.getElementById("meninas_i").style.zIndex='100';
+    document.getElementById("vulcano").style.zIndex="10";
+    document.getElementById("vulcano_i").style.zIndex='10';
+    document.getElementById("inocencio").style.zIndex="10";
+    document.getElementById("inocencio_i").style.zIndex='10';
+}
+function vulcano_d() {
+    document.getElementById("breda").style.zIndex="10";
+    document.getElementById("breda_i").style.zIndex='10';
+    document.getElementById("venus").style.zIndex="10";
+    document.getElementById("venus_i").style.zIndex='10';
+    document.getElementById("meninas").style.zIndex="10";
+    document.getElementById("meninas_i").style.zIndex='10';
+    document.getElementById("vulcano").style.zIndex="100";
+    document.getElementById("vulcano_i").style.zIndex='100';
+    document.getElementById("inocencio").style.zIndex="10";
+    document.getElementById("inocencio_i").style.zIndex='10';
+}
+function inocencio_d() {
+    document.getElementById("breda").style.zIndex="10";
+    document.getElementById("breda_i").style.zIndex='10';
+    document.getElementById("venus").style.zIndex="10";
+    document.getElementById("venus_i").style.zIndex='10';
+    document.getElementById("meninas").style.zIndex="10";
+    document.getElementById("meninas_i").style.zIndex='10';
+    document.getElementById("vulcano").style.zIndex="10";
+    document.getElementById("vulcano_i").style.zIndex='10';
+    document.getElementById("inocencio").style.zIndex="100";
+    document.getElementById("inocencio_i").style.zIndex='100';
+}
